ef016291ed4dba5bf82088894712c703aa6101ef
Tee Guang Ying Marcus <guangyingtgy@live.com>
2023-04-25 05:25:13 +0000

    fixed the application

--- pages/api/transaction.ts
+++ pages/api/transaction.ts
@@ -59,4 +59,57 @@ export default async function handler(req: any, res: any) {
   // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
   //If the request is post, then execute sql prodcedure to insert a new transaction, with the data from the request body
   //The request body has the properties of username, product, quantity and transact_date
+  else if (req.method === "POST") {
+    try {
+      // Execute the stored procedure
+      const result: sql.IProcedureResult<any> = await pool
+        .request()
+        .input("username", sql.NVarChar(50), req.body.username)
+        .input("product", sql.NVarChar(50), req.body.product)
+        .input("quantity", sql.Int, req.body.quantity)
+        .input("transact_date", sql.DateTime, req.body.transact_date)
+        .execute("[dbo].[InsertTransaction]");
+      // Send back the result as JSON
+      res.status(200).json(result.recordset);
+    } catch (err: any) {
+      // Handle errors
+      res.status(500).send(err.message);
+    }
+  }
+  // If the request is put, then execute sql prodcedure to update a transaction, with the data from the request body
+  // The request body has the properties of transaction_id, username, product, quantity and transact_date
+  else if (req.method === "PUT") {
+    try {
+      // Execute the stored procedure
+      const result: sql.IProcedureResult<any> = await pool
+        .request()
+        .input("transaction_id", sql.Int, req.body.transaction_id)
+        .input("username", sql.NVarChar(50), req.body.username)
+        .input("product", sql.NVarChar(50), req.body.product)
+        .input("quantity", sql.Int, req.body.quantity)
+        .input("transact_date", sql.DateTime, req.body.transact_date)
+        .execute("[dbo].[UpdateTransaction]");
+      // Send back the result as JSON
+      res.status(200).json(result.recordset);
+    } catch (err: any) {
+      // Handle errors
+      res.status(500).send(err.message);
+    }
+  }
+  // If the request is delete, then execute sql prodcedure to delete a transaction, with the data from the request query called transaction_id
+  else if (req.method === "DELETE") {
+    try {
+      // Execute the stored procedure
+      const result: sql.IProcedureResult<any> = await pool
+        .request()
+        .input("transaction_id", sql.Int, req.query.transaction_id)
+        .execute("[dbo].[DeleteTransaction]");
+      // Send back the result as JSON
+      res.status(200).json(result.recordset);
+    } catch (err: any) {
+      // Handle errors
+      res.status(500).send(err.message);
+    }
+  }
+
 }
